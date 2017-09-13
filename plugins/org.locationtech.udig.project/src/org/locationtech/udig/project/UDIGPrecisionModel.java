2a2c827c6b1809dbfafd28d3612e35a7133a7b73
Nikolaos Pringouris <nprigour@gmail.com>
2017-09-13 11:41:34 +0300

    UDIGPrecisionFramework

---
+++ plugins/org.locationtech.udig.project/src/org/locationtech/udig/project/UDIGPrecisionModel.java
@@ -0,0 +1,51 @@
+package org.locationtech.udig.project;
+
+import com.vividsolutions.jts.geom.PrecisionModel;
+import com.vividsolutions.jts.precision.GeometryPrecisionReducer;
+
+/**
+ * Can be used to obtain an appropriate PrecisionModel (and associated
+ * GeometryPrecisionReducer based on the value specified in the system 
+ * property "udig.feature.geom.precision". If value  is not set or is invalid
+ * then null values are returned. 
+ * The provided value should represent the number of desired decimal digits.
+ * 
+ * @author Nikolaos Pringouris <nprigour@gmail.com>
+ *
+ */
+public class UDIGPrecisionModel {
+	
+	public static final String UDIG_GEOM_PRECISION_PROPERTY = "udig.geom.precision";
+	
+	private static PrecisionModel model = null;
+	private static GeometryPrecisionReducer reducer = null;
+	
+	static {
+		try {
+			Integer precision = new Integer(System.getProperty(UDIG_GEOM_PRECISION_PROPERTY));
+			if (precision > 0) {
+				model = new PrecisionModel(Math.pow(10, precision));
+				reducer = new GeometryPrecisionReducer(model);
+			}
+		} catch (Exception e) {
+			//
+		}
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static PrecisionModel getModel() {
+		return model;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public static GeometryPrecisionReducer getPrecisionReducer() {
+		return reducer;
+	}
+	
+}
