af8d9ac76d1bb47169d7a9d56d9b240f4249e338
Nikolaos Pringouris <nprigour@gmail.com>
2019-03-19 18:32:10 +0200

    override onMouse... methods to provide some information in the status bar during feature editing
    Signed-off-by: Nikolaos Pringouris <nprigour@gmail.com>

--- plugins/org.locationtech.udig.tool.edit/src/org/locationtech/udig/tools/edit/impl/PolygonTool.java
+++ plugins/org.locationtech.udig.tool.edit/src/org/locationtech/udig/tools/edit/impl/PolygonTool.java
@@ -9,9 +9,17 @@
  */
 package org.locationtech.udig.tools.edit.impl;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Set;
 
+import javax.measure.Measure;
+import javax.measure.quantity.Length;
+import javax.measure.unit.NonSI;
+import javax.measure.unit.SI;
+
+import org.locationtech.udig.catalog.util.CRSUtil;
+import org.locationtech.udig.project.ui.render.displayAdapter.MapMouseEvent;
 import org.locationtech.udig.tool.edit.internal.Messages;
 import org.locationtech.udig.tools.edit.AbstractEditTool;
 import org.locationtech.udig.tools.edit.Activator;
@@ -44,12 +52,19 @@ import org.locationtech.udig.tools.edit.behaviour.accept.AcceptChangesBehaviour;
 import org.locationtech.udig.tools.edit.behaviour.accept.DeselectEditShapeAcceptBehaviour;
 import org.locationtech.udig.tools.edit.enablement.ValidToolDetectionActivator;
 import org.locationtech.udig.tools.edit.enablement.WithinLegalLayerBoundsBehaviour;
+import org.locationtech.udig.tools.edit.support.Point;
+import org.locationtech.udig.tools.edit.support.PrimitiveShape;
 import org.locationtech.udig.tools.edit.support.ShapeType;
 import org.locationtech.udig.tools.edit.validator.PolygonCreationValidator;
-
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.SWT;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+import org.geotools.geometry.jts.JTS;
 import org.opengis.filter.spatial.Intersects;
 
+import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.MultiPolygon;
 import org.locationtech.jts.geom.Polygon;
@@ -140,5 +155,139 @@ public class PolygonTool extends AbstractEditTool {
         activators.add(new AdvancedBehaviourCommandHandlerActivator());
         activators.add(new GridActivator());
    }
+
+    
+    
+	/* (non-Javadoc)
+	 * @see org.locationtech.udig.tools.edit.AbstractEditTool#onMouseDragged(org.locationtech.udig.project.ui.render.displayAdapter.MapMouseEvent)
+	 */
+	@Override
+	protected void onMouseDragged(MapMouseEvent e) {
+		super.onMouseDragged(e);
+		computeAndDisplayEdgeLenghtInfo(e);
+	}
+
+
+	/**
+	 * @see org.locationtech.udig.tools.edit.AbstractEditTool#onMouseMoved(org.locationtech.udig.project.ui.render.displayAdapter.MapMouseEvent)
+	 */
+	@Override
+	protected void onMouseMoved(MapMouseEvent e) {
+		super.onMouseMoved(e);
+		computeAndDisplayEdgeLenghtInfo(e);
+	}
+
+	/**
+	 * 
+	 * @param e
+	 */
+	private void computeAndDisplayEdgeLenghtInfo(MapMouseEvent e) {
+		try {
+			final Coordinate current = getContext().pixelToWorld(e.x, e.y);
+			PrimitiveShape shape = getHandler().getCurrentShape();
+			if (shape != null) {
+				final Coordinate[] coords = shape.coordArray();
+				displayOnStatusBar(
+						JTS.orthodromicDistance(coords[coords.length-1], current, getContext().getCRS()),
+						JTS.orthodromicDistance(current, coords[0], getContext().getCRS()));
+			}		
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}		
+	}
+	
+	/**
+	 * 
+	 * @param distanceFrom
+	 * @param distanceTo
+	 */
+    private void displayOnStatusBar( double distanceFrom, double distanceTo ) {
+        final IStatusLineManager statusBar = getContext().getActionBars().getStatusLineManager();
+
+        if (statusBar == null)
+            return; // shouldn't happen if the tool is being used.
+
+        IPreferenceStore preferenceStore = new ScopedPreferenceStore(InstanceScope.INSTANCE, "org.locationtech.udig.ui");
+
+        String units = preferenceStore.getString(org.locationtech.udig.ui.preferences.PreferenceConstants.P_DEFAULT_UNITS);
+        if (units.equals( org.locationtech.udig.ui.preferences.PreferenceConstants.AUTO_UNITS) && CRSUtil.isCoordinateReferenceSystemImperial(context.getCRS())){
+            units = org.locationtech.udig.ui.preferences.PreferenceConstants.IMPERIAL_UNITS;
+        }
+
+        final Measure<Double, Length> distanceFromInMeter = Measure.valueOf(distanceFrom, SI.METER);
+        final Measure<Double, Length> distanceToInMeter = Measure.valueOf(distanceTo, SI.METER);
+        
+        Measure<Double, Length> resultFrom = null;
+        Measure<Double, Length> resultTo = null;
+        if (units.equals( org.locationtech.udig.ui.preferences.PreferenceConstants.IMPERIAL_UNITS)){
+            
+        	Measure<Double, Length> distanceFromInMiles = distanceFromInMeter.to(NonSI.MILE);
+            double distInMilesValue = distanceFromInMiles.getValue().doubleValue();
+            if (distInMilesValue > Measure.valueOf(1, NonSI.MILE).doubleValue(NonSI.MILE)) {
+                // everything longer than a mile
+                resultFrom = distanceFromInMiles;
+            } else if (distInMilesValue > Measure.valueOf(1, NonSI.FOOT).doubleValue(NonSI.MILE)) {
+                // everything longer that a foot
+                resultFrom = distanceFromInMiles.to(NonSI.FOOT);
+            } else {
+                // shorter than a foot
+                resultFrom = distanceFromInMiles.to(NonSI.INCH);
+            }
+            
+            Measure<Double, Length> distanceToInMiles = distanceToInMeter.to(NonSI.MILE);
+            double distanceToInMilesValue = distanceToInMiles.getValue().doubleValue();
+            if (distanceToInMilesValue > Measure.valueOf(1, NonSI.MILE).doubleValue(NonSI.MILE)) {
+                // everything longer than a mile
+            	resultTo = distanceToInMiles;
+            } else if (distanceToInMilesValue > Measure.valueOf(1, NonSI.FOOT).doubleValue(NonSI.MILE)) {
+                // everything longer that a foot
+            	resultTo = distanceToInMiles.to(NonSI.FOOT);
+            } else {
+                // shorter than a foot
+            	resultTo = distanceToInMiles.to(NonSI.INCH);
+            }
+        } else {
+            double distanceFromInMeterValue = distanceFromInMeter.getValue().doubleValue();       
+            if (distanceFromInMeterValue > Measure.valueOf(1000, SI.METER).doubleValue(SI.METER)) {
+                resultFrom = distanceFromInMeter.to(SI.KILOMETER);
+            } else if (distanceFromInMeterValue > Measure.valueOf(1, SI.METER).doubleValue(SI.METER)) {
+                resultFrom = distanceFromInMeter.to(SI.METER);
+            } else if (distanceFromInMeterValue > Measure.valueOf(1, SI.CENTIMETER).doubleValue(SI.METER)) {
+                resultFrom = distanceFromInMeter.to(SI.CENTIMETER);
+            } else {
+                resultFrom = distanceFromInMeter.to(SI.MILLIMETER);
+            }
+            
+            double distanceToInMeterValue = distanceToInMeter.getValue().doubleValue();
+            if (distanceToInMeterValue > Measure.valueOf(1000, SI.METER).doubleValue(SI.METER)) {
+            	resultTo = distanceToInMeter.to(SI.KILOMETER);
+            } else if (distanceToInMeterValue > Measure.valueOf(1, SI.METER).doubleValue(SI.METER)) {
+            	resultTo = distanceToInMeter.to(SI.METER);
+            } else if (distanceToInMeterValue > Measure.valueOf(1, SI.CENTIMETER).doubleValue(SI.METER)) {
+            	resultTo = distanceToInMeter.to(SI.CENTIMETER);
+            } else {
+            	resultTo = distanceToInMeter.to(SI.MILLIMETER);
+            }
+        }
+
+        final String message = MessageFormat.format("Distance (previous point:{0}, next point: {1}", 
+        		round(resultFrom.getValue(), 3) + " " + resultFrom.getUnit(), 
+        		round(resultTo.getValue(), 3) + " " + resultTo.getUnit());
+
+        getContext().updateUI(new Runnable(){
+            public void run() {
+                statusBar.setErrorMessage(null);
+                statusBar.setMessage(message);
+            }
+        });
+    }
+    
+    
+    private double round(double value, int decimalPlaces) {
+        double divisor = Math.pow(10, decimalPlaces);
+        double newVal = value * divisor;
+        newVal =  (Long.valueOf(Math.round(newVal)).intValue())/divisor;
+        return newVal;
+    }
 
 }
--- plugins/org.locationtech.udig.tool.edit/src/org/locationtech/udig/tools/edit/impl/SelectionTool.java
+++ plugins/org.locationtech.udig.tool.edit/src/org/locationtech/udig/tools/edit/impl/SelectionTool.java
@@ -9,9 +9,16 @@
  */
 package org.locationtech.udig.tools.edit.impl;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Set;
 
+import javax.measure.Measure;
+import javax.measure.quantity.Length;
+import javax.measure.unit.NonSI;
+import javax.measure.unit.SI;
+
+import org.locationtech.udig.catalog.util.CRSUtil;
 import org.locationtech.udig.core.StaticProvider;
 import org.locationtech.udig.tool.edit.internal.Messages;
 import org.locationtech.udig.tools.edit.AbstractEditTool;
@@ -43,11 +50,19 @@ import org.locationtech.udig.tools.edit.behaviour.SetSnapSizeBehaviour;
 import org.locationtech.udig.tools.edit.behaviour.accept.AcceptChangesBehaviour;
 import org.locationtech.udig.tools.edit.enablement.ValidToolDetectionActivator;
 import org.locationtech.udig.tools.edit.enablement.WithinLegalLayerBoundsBehaviour;
-
+import org.locationtech.udig.tools.edit.support.PrimitiveShape;
+import org.locationtech.udig.tools.edit.support.Selection;
+import org.locationtech.udig.tools.edit.support.ShapeType;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.SWT;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+import org.geotools.geometry.jts.JTS;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.filter.spatial.Intersects;
 
+import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.LineString;
 import org.locationtech.jts.geom.MultiLineString;
@@ -164,6 +179,175 @@ public class SelectionTool extends AbstractEditTool {
         helper.add( new AcceptOnDoubleClickBehaviour() );
         helper.add( new SetSnapSizeBehaviour());     
         helper.done();
+    }
+
+    
+	/* (non-Javadoc)
+	 * @see org.locationtech.udig.tools.edit.AbstractEditTool#onMouseDragged(org.locationtech.udig.project.ui.render.displayAdapter.MapMouseEvent)
+	 */
+	@Override
+	protected void onMouseDragged(MapMouseEvent e) {
+		super.onMouseDragged(e);
+		computeAndDisplayEdgeLenghtInfo(e);
+	}
+
+
+	/**
+	 * @see org.locationtech.udig.tools.edit.AbstractEditTool#onMouseMoved(org.locationtech.udig.project.ui.render.displayAdapter.MapMouseEvent)
+	 */
+	@Override
+	protected void onMouseMoved(MapMouseEvent e) {
+		super.onMouseMoved(e);
+		//computeAndDisplayEdgeLenghtInfo(e);
+	}
+
+	/**
+	 * 
+	 * @param e
+	 */
+	private void computeAndDisplayEdgeLenghtInfo(MapMouseEvent e) {
+		try {
+			Selection selection = handler.getEditBlackboard(handler.getEditLayer()).getSelection();
+			if (selection.size() != 1) {
+				return;
+			}
+			ShapeType type = getHandler().getCurrentGeom().getShapeType();
+			if (type == ShapeType.LINE || type == ShapeType.POLYGON) {	
+				final Coordinate current = getContext().pixelToWorld(e.x, e.y);
+				PrimitiveShape shape = getHandler().getCurrentShape();
+				if (shape != null) {
+					int index = shape.getPoints().indexOf(selection.iterator().next());
+					if (index > -1) {	
+						int totalPoints = shape.getPoints().size();
+						//final Coordinate[] coords = shape.coordArray();
+						int previousIndex = index > 0 ? index-1 : -1;
+						int nextIndex = index < totalPoints-1 ? index+1 : -1;
+						if (type == ShapeType.POLYGON) {
+							previousIndex = previousIndex != -1 ? previousIndex : totalPoints-2;
+							nextIndex = nextIndex != -1 ? nextIndex : 0;
+						}
+						//Coordinate previous = shape.getCoordsAt(shape.getPoint(previousIndex)).get(0);
+						//Coordinate after = shape.getCoordsAt(shape.getPoint(nextIndex)).get(0);
+						Coordinate previous = previousIndex != -1 ? getContext().pixelToWorld(
+								shape.getPoint(previousIndex).getX(), shape.getPoint(previousIndex).getY()) : null;
+						Coordinate after = nextIndex != -1 ? getContext().pixelToWorld(
+								shape.getPoint(nextIndex).getX(), shape.getPoint(nextIndex).getY()) : null;
+						Double distanceFrom =  previous != null ? 
+								JTS.orthodromicDistance(previous, current, getContext().getCRS()) : null;
+						Double distanceTo = after != null ? 
+								JTS.orthodromicDistance(current, after, getContext().getCRS()) : null;
+						displayOnStatusBar(distanceFrom, distanceTo);
+					}
+				}
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}		
+	}
+	
+	/**
+	 * 
+	 * @param distanceFrom
+	 * @param distanceTo
+	 */
+    private void displayOnStatusBar( Double distanceFrom, Double distanceTo ) {
+        final IStatusLineManager statusBar = getContext().getActionBars().getStatusLineManager();
+
+        if (statusBar == null)
+            return; // shouldn't happen if the tool is being used.
+
+        IPreferenceStore preferenceStore = new ScopedPreferenceStore(InstanceScope.INSTANCE, "org.locationtech.udig.ui");
+
+        String units = preferenceStore.getString(org.locationtech.udig.ui.preferences.PreferenceConstants.P_DEFAULT_UNITS);
+        if (units.equals( org.locationtech.udig.ui.preferences.PreferenceConstants.AUTO_UNITS) && CRSUtil.isCoordinateReferenceSystemImperial(context.getCRS())){
+            units = org.locationtech.udig.ui.preferences.PreferenceConstants.IMPERIAL_UNITS;
+        }
+
+        final Measure<Double, Length> distanceFromInMeter = 
+        		distanceFrom != null ? Measure.valueOf(distanceFrom, SI.METER) : null;
+        final Measure<Double, Length> distanceToInMeter = 
+        		distanceTo != null ? Measure.valueOf(distanceTo, SI.METER) : null;
+        
+        Measure<Double, Length> resultFrom = null;
+        Measure<Double, Length> resultTo = null;
+        if (units.equals( org.locationtech.udig.ui.preferences.PreferenceConstants.IMPERIAL_UNITS)){
+            
+        	if (distanceFrom != null) {
+	        	Measure<Double, Length> distanceFromInMiles = distanceFromInMeter.to(NonSI.MILE);
+	            double distInMilesValue = distanceFromInMiles.getValue().doubleValue();
+	            if (distInMilesValue > Measure.valueOf(1, NonSI.MILE).doubleValue(NonSI.MILE)) {
+	                // everything longer than a mile
+	                resultFrom = distanceFromInMiles;
+	            } else if (distInMilesValue > Measure.valueOf(1, NonSI.FOOT).doubleValue(NonSI.MILE)) {
+	                // everything longer that a foot
+	                resultFrom = distanceFromInMiles.to(NonSI.FOOT);
+	            } else {
+	                // shorter than a foot
+	                resultFrom = distanceFromInMiles.to(NonSI.INCH);
+	            }
+        	}
+            
+        	if (distanceTo != null) {
+	            Measure<Double, Length> distanceToInMiles = distanceToInMeter.to(NonSI.MILE);
+	            double distanceToInMilesValue = distanceToInMiles.getValue().doubleValue();
+	            if (distanceToInMilesValue > Measure.valueOf(1, NonSI.MILE).doubleValue(NonSI.MILE)) {
+	                // everything longer than a mile
+	            	resultTo = distanceToInMiles;
+	            } else if (distanceToInMilesValue > Measure.valueOf(1, NonSI.FOOT).doubleValue(NonSI.MILE)) {
+	                // everything longer that a foot
+	            	resultTo = distanceToInMiles.to(NonSI.FOOT);
+	            } else {
+	                // shorter than a foot
+	            	resultTo = distanceToInMiles.to(NonSI.INCH);
+	            }
+        	}
+        } else {
+        	if (distanceFrom != null) {
+	            double distanceFromInMeterValue = distanceFromInMeter.getValue().doubleValue();       
+	            if (distanceFromInMeterValue > Measure.valueOf(1000, SI.METER).doubleValue(SI.METER)) {
+	                resultFrom = distanceFromInMeter.to(SI.KILOMETER);
+	            } else if (distanceFromInMeterValue > Measure.valueOf(1, SI.METER).doubleValue(SI.METER)) {
+	                resultFrom = distanceFromInMeter.to(SI.METER);
+	            } else if (distanceFromInMeterValue > Measure.valueOf(1, SI.CENTIMETER).doubleValue(SI.METER)) {
+	                resultFrom = distanceFromInMeter.to(SI.CENTIMETER);
+	            } else {
+	                resultFrom = distanceFromInMeter.to(SI.MILLIMETER);
+	            }
+        	}
+        	
+        	if (distanceTo != null) {
+	            double distanceToInMeterValue = distanceToInMeter.getValue().doubleValue();
+	            if (distanceToInMeterValue > Measure.valueOf(1000, SI.METER).doubleValue(SI.METER)) {
+	            	resultTo = distanceToInMeter.to(SI.KILOMETER);
+	            } else if (distanceToInMeterValue > Measure.valueOf(1, SI.METER).doubleValue(SI.METER)) {
+	            	resultTo = distanceToInMeter.to(SI.METER);
+	            } else if (distanceToInMeterValue > Measure.valueOf(1, SI.CENTIMETER).doubleValue(SI.METER)) {
+	            	resultTo = distanceToInMeter.to(SI.CENTIMETER);
+	            } else {
+	            	resultTo = distanceToInMeter.to(SI.MILLIMETER);
+	            }
+        	}
+        }
+
+        final String message = MessageFormat.format("Distance ({0}{1})", 
+        		distanceFrom != null ? 
+        				"previous point: " + round(resultFrom.getValue(), 3) + " " + resultFrom.getUnit() + ", " : "", 
+        		distanceTo != null ? "next point: " + round(resultTo.getValue(), 3) + " " + resultTo.getUnit() : "");
+
+        getContext().updateUI(new Runnable(){
+            public void run() {
+                statusBar.setErrorMessage(null);
+                statusBar.setMessage(message);
+            }
+        });
+    }
+    
+    
+    private double round(double value, int decimalPlaces) {
+        double divisor = Math.pow(10, decimalPlaces);
+        double newVal = value * divisor;
+        newVal =  (Long.valueOf(Math.round(newVal)).intValue())/divisor;
+        return newVal;
     }
 
 }
